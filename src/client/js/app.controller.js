def79792c12e1967b904293d371bcd659cae2921
Hohn Rudolf <rudolf.hohn@gmail.com>
2016-11-13 14:37:58 +0100

    feat(angular): add angular to the template
    Angular code is present and working.
    STServices ready to test.

---
+++ src/client/js/app.controller.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('AppController', AppController)
+        ;
+
+    AppController.$inject = ['STServices'];
+    function AppController(STServices) {
+        var vm = this;
+
+        
+    }
+
+})();
